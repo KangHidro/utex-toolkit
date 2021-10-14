37b53847f67c6b35fbadafcec4ca826fd248d82c
duyll <duyll@hcmute.edu.vn>
2021-10-14 16:16:31 +0700

    fix window alert error

--- src/app/core/constant.ts
+++ src/app/core/constant.ts
@@ -5,5 +5,5 @@ export class SystemConstant {
     OTHER: 'OTHER',
   };
 
-  public static JS_FOR_VIDEO_TYPE = '<script>function attachLink(t,e){"complete"===document.readyState?document.getElementById(t).src=e:alert("Dữ liệu vẫn đang tải...\nVui lòng bấm OK và chờ thêm giây lát!")}</script>';
+  public static JS_FOR_VIDEO_TYPE = '<script>function attachLink(t,e){"complete"===document.readyState?document.getElementById(t).src=e:alert(`Dữ liệu vẫn đang tải...\nVui lòng bấm OK và chờ thêm giây lát!`)}</script>';
 }
